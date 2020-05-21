29f276d669a8283da261321dbd966c39a9b2f488
hhthuonghcmus <65527547+hhthuonghcmus@users.noreply.github.com>
2020-05-21 17:48:30 +0700

    Update snake.cpp

--- snake.cpp
+++ snake.cpp
@@ -6,6 +6,7 @@ SNAKE* initSnake() {
 	snake->body = (POS*)malloc(snake->length * sizeof(POS));
 	snake->body[0].x = 5;
 	snake->body[0].y = 5;
+	// sua lai head theo random
 	snake->body[0].c = '1';
 	snake->dir = 0;
 	snake->tmpDir = 0;
@@ -15,23 +16,30 @@ SNAKE* initSnake() {
 
 void pushTopTail(SNAKE* snake, POS newTail) {
 	POS* newBody = (POS*)realloc(snake->body, (snake->length + 1) * sizeof(POS));
-	for (int i = 2; i < snake->length + 1; i++) {
-		snake->body[i].x = snake->body[i - 1].x;
-		snake->body[i].y = snake->body[i - 1].y;
-	}
 	if (newBody != NULL) {
+		newBody[snake->length].c = newTail.c;
+		for (int i = snake->length; i >= 2; i--) {
+			newBody[i].x = newBody[i - 1].x;
+			newBody[i].y = newBody[i - 1].y;
+		}
+		newBody[1].x = newTail.x;
+		newBody[1].y = newTail.y;
 		snake->body = newBody;
-		snake->body[snake->length].x = newTail.x;
-		snake->body[snake->length].y = newTail.y;
-		snake->body[snake->length].c = newTail.c;
 		snake->length++;
 	}
-
 }
 
 void renderSnake(SNAKE* snake) {
 	drawChar(snake->body[0].x, snake->body[0].y, headColor, snake->body[0].c);
 	for (int i = 1; i < snake->length; i++) {
 		drawChar(snake->body[i].x, snake->body[i].y, tailColor, snake->body[i].c);
+	}
+}
+
+void deleteSnake(SNAKE* snake) {
+	if (snake != NULL)
+	{
+		delete[]snake->body;
+		delete[]snake;
 	}
 }
