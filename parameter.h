7b6e092a793600168b4e6ea2cccae4fde3f56a81
hhthuonghcmus <65527547+hhthuonghcmus@users.noreply.github.com>
2020-05-22 08:36:23 +0700

    Update parameter.h

--- parameter.h
+++ parameter.h
@@ -14,9 +14,6 @@ using namespace std;
 void FixConsoleWindow();
 void GotoXY(int x, int y);
 void TextColor(int color);
-
-
-// Ham ve 
 #define headColor 11
 #define tailColor 12
 #define pauseGame    0x50 // P
@@ -30,10 +27,9 @@ void TextColor(int color);
 #define red            12
 #define green          10
 #define WIDTH_CONSOLE  10
-#define HEIGH_CONSOLE 10
-const char fruit_name[] =  "812058618120609181205981812032218120316";
+#define HEIGHT_CONSOLE 10
+const char fruit_name[] = "812058618120609181205981812032218120316";
 void drawChar(int x, int y, int color, int character);
 void drawStr(int x, int y, int color, string longString);
-
-
-
+bool IsValid(SNAKE* snake, int x, int y);
+void GenerateFruit(SNAKE* snake, POS& fruit, int& vt);
