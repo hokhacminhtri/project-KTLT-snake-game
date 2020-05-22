a69f27abdb726f170a5bac8b7bc4c4fcf9f793a7
hhthuonghcmus <65527547+hhthuonghcmus@users.noreply.github.com>
2020-05-22 08:35:29 +0700

    Update game.h

--- game.h
+++ game.h
@@ -6,8 +6,5 @@ void Loading();
 void Menu();
 void NewGame();
 int Input(SNAKE* snake);
-void Update(SNAKE* snake, POS& fruit,int& vt);
+void Update(SNAKE* snake, POS& fruit, int& vt);
 void Render(SNAKE* snake, POS& fruit);
-//ham kiem tra vt fruit co trung voi snake
-bool IsValid(SNAKE* snake, int x, int y);
-void GenerateFruit(SNAKE* snake, POS& fruit,int &vt);
